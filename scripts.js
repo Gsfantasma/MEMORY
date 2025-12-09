fed7f4cf620025d4a3615587e58850e0f730e3f5
Glauco Adenauer <gsfantasma.dev@gmail.com>
2025-12-08 21:39:59 -0300

    "Adição de emoji + função de embaralhar"

--- scripts.js
+++ scripts.js
@@ -4,3 +4,41 @@ let flippedCards = [] // Array que armazena as cartas viradas (sempre terá no m
 let matchedPairs = 0 // Contador de pares encontrados.
 let attempts = 0// Contador de tentativas do jogador.
 let isCheckingPair = false // Trava o jogo enquanto verifica o par ou esconde as cartas.
+
+// Array com todas as cartas do jogo
+const cardItems = [
+    {id: 1, content: "🚀", matched: false },
+    {id: 2, content: "🚀", matched: false },
+    {id: 3, content: "😎", matched: false },
+    {id: 4, content: "😎", matched: false },
+    {id: 5, content: "🚢", matched: false },
+    {id: 6, content: "🚢", matched: false },
+    {id: 7, content: "🐄", matched: false },
+    {id: 8, content: "🐄", matched: false },
+    {id: 9, content: "🍰", matched: false },
+    {id: 10, content: "🍰", matched: false },
+    {id: 11, content: "☂️", matched: false },
+    {id: 12, content: "☂️", matched: false },
+    {id: 13, content: "🍊", matched: false },
+    {id: 14, content: "🍊", matched: false },
+    {id: 15, content: "🦕", matched: false },
+    {id: 16, content: "🦕", matched: false },
+
+]
+// Função que emabaralha as cartas.
+function shuffleCards(array) {
+    // Positivo vai depois, Negativo vai antes.
+    const shuffled = array.sort(() => Math.random() > 0.5 ? 1 : -1) 
+    
+    return shuffled
+}
+
+function renderCards() {
+    const deck = document.getElementById("deck")
+    deck.innerHTML = ""
+
+    const cards = shuffleCards(cardItems)
+    cards.forEach((item) => console.log(item))
+}
+
+renderCards()
